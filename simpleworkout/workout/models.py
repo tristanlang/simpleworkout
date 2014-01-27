9746a09d6ff5de32f3e52e984517a8e2f17cdd1e
tristanjlang <tristan@alum.mit.edu>
2014-01-26 23:05:46 -0500

    Tristan added initial models and modified a few templates based on updates to models

--- simpleworkout/workout/models.py
+++ simpleworkout/workout/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class WorkoutCategories(models.Model):
+    category = models.CharField(max_length=20)
+
+class Workouts(models.Model):
+    date = models.DateTimeField('date of workout')
+    category = models.ForeignKey(WorkoutCategories)
+    detail = models.TextField()
+    notes = models.CharField(max_length=200)
+
+class Equipment(models.Model):
+    item = models.CharField(max_length=100)
+
+class Users(models.Model):
+    username = models.CharField(max_length=20)
+    password_hash = models.TextField()
+
+class Preferences(models.Model):
+    user = models.ForeignKey(Users)
+    days_per_week = models.PositiveSmallIntegerField()
+    category = models.ForeignKey(WorkoutCategories)
+
+class Ownership(models.Model)
+    user = models.ForeignKey(Users)
+    equipment = models.ForeignKey(Equipment)
