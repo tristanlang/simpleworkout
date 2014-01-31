3bfb45e0f9a7820ee0615e7111694dd4e1f5c0a8
tristanjlang <tristan@alum.mit.edu>
2014-01-30 22:38:59 -0500

    reinitialize hard reset

--- simpleworkout/simpleworkout/wsgi.py
+++ simpleworkout/simpleworkout/wsgi.py
@@ -11,4 +11,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simpleworkout.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#application = get_wsgi_application() #changing for production
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
